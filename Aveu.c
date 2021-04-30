f6114340a0ee8c9c8426ddb170eb7836f567b6d1
marinarx20 <47271160+marinarx20@users.noreply.github.com>
2021-04-30 15:51:58 +0200

    Update Aveu.c

--- Aveu.c
+++ Aveu.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include<string.h>
 
+int horarioManiana();
+
 int main(){
    
    
 	FILE * pfichero1;
+	FILE * pfichero2;
    	
-    int opcion,i,longitud=0,nbilletes,edad,opcion2,salir,j,equipaje,bultos;
+    int opcion,i,longitud=0,nbilletes,edad,opcion2,opcion3,hora,salir,j,equipaje,bultos;
     float precio=30,precio2,descuento,peso;
-    char name[12],nameOriginal[25],destino,origen[30],destino1[30];
+    char name[12],nameOriginal[25],destino,origen[30],destino1[30],precioEquipaje;
    
 	do{
-    printf("\n\nMENU");
-    printf("\n\n1. Registro del usuario.");
+    printf("\nMENU");
+    printf("\n1. Registro del usuario.");
     printf("\n2. Mostrar todos los recorridos");
     printf("\n3. Compra de billetes de tren de ida y vuelta");
     printf("\n4. Compra de billetes de tren de ida");
-    printf("\n5. Salir\n");
+    printf("\n5. Ver precio del equipaje.");
+    printf("\n6. Salir\n");
     scanf("%d",&opcion);
     switch(opcion){
     case 1:
+    	system("cls");
     	do{
    		printf("Introduce un nombre de usuario sin espacios:");
     	scanf("%s",name);
@@ -35,9 +40,11 @@ int main(){
     else{
 	printf("\nBienvenid@ %s, nombre de usuario correcto.\n",name);
 	}
+	
 	break;
 	
 	case 2:
+		system("cls");
 		pfichero1=fopen("destinos.txt","r");
     	if(pfichero1==NULL){
     	printf("Error en la apertura del fichero\n");
@@ -48,7 +55,10 @@ int main(){
 		printf("%c",destino);
 		}
 		fclose(pfichero1);
+		getch();
+		break;
 	case 3:
+		system("cls");
 		printf("Introduzca la ciudad de origen:");
 		scanf("%s",origen);
 		printf("Introduzca el numero de billetes que deseas comprar:");
@@ -68,13 +78,13 @@ int main(){
   		descuento=(precio2/nbilletes)*0.5;
   		precio -= descuento;
   	 	printf("\nBillete/s de ida: %.2f euros actualmente\n",precio);
-  		}else if (edad>=6 && edad<=13){
+  		}else if (edad>=6 && edad<=12){
   		printf("\nSe aplica la Tarifa Children\n");
   		printf("\nSe aplica un descuento del 30 por ciento\n");
   		descuento=(precio2/nbilletes)*0.3;
   		precio-= descuento;
   		printf("\nBillete/s de ida: %.2f euros actualmente\n",precio);
-  		}else if(edad>=14 && edad<=65){
+  		}else if(edad>=13 && edad<=60){
   		printf("\nNo se aplican descuentos\n");
   		printf("\nBillete/s de ida: %.2f euros actualmente\n",precio);
   		}else{
@@ -103,18 +113,18 @@ int main(){
         }
     }while(bultos>2);
     
-         printf("\n Introduzca la informacion de su equipaje\n");
+        printf("\n Introduzca la informacion de su equipaje\n");
     
       for(j=0;j<bultos;j++){
        printf("Introduzca el peso en kg del bulto %d: ",j+1);
        scanf("%f",&peso);
-       if(peso>=8&&peso<15){
+       if(peso>=5&&peso<9){
        precio+=10; 
         }
-       if(peso>=15&&peso<25){
+       if(peso>=9&&peso<15){
        precio+=20;
         }
-       if(peso>=25&&peso<=35){
+       if(peso>=15&&peso<=25){
        precio+=30; 
         }
        printf("\nBillete de ida: %.2f euros actualmente\n",precio);
@@ -142,8 +152,9 @@ int main(){
  	printf("Usted ha elegido la opcion %d\n",opcion2);
 	}while(opcion2<1 ||opcion2>3);
 	
-
+	break;
 	case 4:
+		system("cls");
 		printf("Introduzca la ciudad de origen:");
 		scanf("%s",origen);
 		printf("Introduzca el numero de billetes que deseas comprar:");
@@ -169,7 +180,7 @@ int main(){
   		descuento=(precio2/nbilletes)*0.3;
   		precio-= descuento;
   		printf("\nBillete/s de ida: %.2f euros actualmente\n",precio);
-  		}else if(edad>=13 && edad<=65){
+  		}else if(edad>=13 && edad<=60){
   		printf("\nNo se aplican descuentos\n");
   		printf("\nBillete/s de ida: %.2f euros actualmente\n",precio);
   		}else{
@@ -203,13 +214,13 @@ int main(){
       for(j=0;j<bultos;j++){
        printf("Introduzca el peso en kg del bulto %d: ",j+1);
        scanf("%f",&peso);
-       if(peso>=8&&peso<15){
+       if(peso>=5&&peso<9){
        precio+=10; 
         }
-       if(peso>=15&&peso<25){
+       if(peso>=9&&peso<15){
        precio+=20;
         }
-       if(peso>=25&&peso<=35){
+       if(peso>=15&&peso<=25){
        precio+=30; 
         }
        printf("\nBillete de ida: %.2f euros actualmente\n",precio);
@@ -235,10 +246,42 @@ int main(){
  
  	scanf("%d",&opcion2);
  	printf("Usted ha elegido la opcion %d\n",opcion2);
-	}while(opcion2<1 ||opcion2>3);
-	
-	
+	}while(opcion2>0 && opcion2<=3);
+	break;
+	switch(opcion3){
+		do{
+			case 1:
+				printf("Escoja la hora del vuelo de por la manania");
+				if(hora==1){
+					printf("Has elegido el vuelo de las 7:30 de la manania");
+				}
+				else if(hora==2){
+					printf("Has elegido el vuelo de las 10:30");
+				}
+				else if(hora==3){
+					printf("Has elegido el vuelo de las 12:30");
+				}
+				break;
+		}while(hora>1 && hora<=3);
+	}
 	case 5:
+		system("cls");
+		pfichero2=fopen("equipaje.txt","r");
+		if(pfichero2==NULL){
+			printf("Error en la apertura del fichero");
+			return 0;
+		}
+		
+		while(fscanf(pfichero2,"%c",&precioEquipaje)!=EOF){
+			printf("%c",precioEquipaje);
+		}
+		fscanf(pfichero2,"%c",precioEquipaje);
+		fclose(pfichero2);
+
+		break;
+	system("cls");
+	case 6:
+		
 		printf("1.SI \n");
 					printf("2.NO \n");
 						do{
@@ -261,5 +304,13 @@ int main(){
 	}
 	
 		
-    }while(opcion<=0||opcion>5);
+    }while(opcion>0 && opcion<=6);
+    return 0;
+}
+int horarioManiana(){
+	int hora;
+	printf("1- 7:30");
+	printf("2- 10:30 ");
+	printf("3- 12:30");
+	
 }
